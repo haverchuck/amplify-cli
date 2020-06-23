54f8941ba7f4e5a54677bf34864e0349ed0eb645
Noyes <dnnoyes@3c22fb0f428c.ant.amazon.com>
2020-06-23 12:07:34 -0700

    feat(amplify-frontend-flutter): adds frontend plugin for flutter

--- packages/amplify-cli/src/domain/amplify-toolkit.ts
+++ packages/amplify-cli/src/domain/amplify-toolkit.ts
@@ -35,6 +35,7 @@ export class AmplifyToolkit {
   private _pushResources: any;
   private _storeCurrentCloudBackend: any;
   private _readJsonFile: any;
+  private _readJsonFromDart: any;
   private _removeEnvFromCloud: any;
   private _removeResource: any;
   private _sharedQuestions: any;
@@ -69,6 +70,7 @@ export class AmplifyToolkit {
   private _getTriggerEnvInputs: any;
   private _getUserPoolGroupList: any;
   private _forceRemoveResource: any;
+  private _writeJsonToDart: any;
   private _writeObjectAsJson: any;
   private _hashDir: any;
   private _leaveBreadcrumbs: any;
@@ -227,6 +229,11 @@ export class AmplifyToolkit {
   get readJsonFile(): any {
     this._readJsonFile = this._readJsonFile || require(path.join(this._amplifyHelpersDirPath, 'read-json-file')).readJsonFile;
     return this._readJsonFile;
+  }
+  get readJsonFromDart(): any {
+    this._readJsonFromDart =
+      this._readJsonFromDart || require(path.join(this._amplifyHelpersDirPath, 'read-json-from-dart')).readJsonFromDart;
+    return this._readJsonFromDart;
   }
   get removeEnvFromCloud(): any {
     this._removeEnvFromCloud =
@@ -403,6 +410,11 @@ export class AmplifyToolkit {
     this._writeObjectAsJson =
       this._writeObjectAsJson || require(path.join(this._amplifyHelpersDirPath, 'write-object-as-json')).writeObjectAsJson;
     return this._writeObjectAsJson;
+  }
+
+  get writeJsonToDart(): any {
+    this._writeJsonToDart = this._writeJsonToDart || require(path.join(this._amplifyHelpersDirPath, 'write-json-to-dart')).writeJsonToDart;
+    return this._writeJsonToDart;
   }
 
   get hashDir(): any {
